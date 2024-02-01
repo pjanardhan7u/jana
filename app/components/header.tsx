835ff6e9fc732b8eec58e76d1c2970f707ca7c7f
P_Janardhan <pjanardhan7u@gmail.com>
2024-02-01 21:17:40 +0530

    navbar updated

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -50,7 +50,7 @@ export default function Header() {
             w-30 px-20 rounded-full border
              dark:border-white border-opacity-40 
              bg-opacity-100 shadow-lg sm:shadow-black/[0.03] 
-            backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[47rem] 
+            backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[50rem] 
              bg-black/70 border-black/40 dark:bg-opacity-75"
             initial={{ y: -100, x: "-50%", opacity: 0 }}
             animate={{ y: 0, x: "-50%", opacity: 1 }}
@@ -59,7 +59,7 @@ export default function Header() {
           <nav className="flex fixed mt-2 sm:mt-0 top-[0.15rem] left-1/2 h-5 sm:h-10 -translate-x-1/2
            sm:top-[1.7rem]  sm:py-0">
             
-            <ul className={clsx("flex w-[18rem] gap-y-1 justify-around  font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-1")}>
+            <ul className={clsx("flex w-[18rem]  gap-y-1 justify-between  font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-1")}>
               {Mlink.map((link) => (
                 
                 <motion.li
